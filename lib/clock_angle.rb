53e65348ab629f7f113aa584529a885a83531912
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-07-30 21:50:47 -0700

    Fix precision, add tests, and add style

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,17 +1,23 @@
 def clock(time)
-	hour = time[0...-3].to_i
-	minute = time[-2..-1].to_i
+	hour = time[0...-3].to_f
+	minute = time[-2..-1].to_f
 
 	if hour >= 12
 		hour = (hour-12)
 	end
 
-	angle = ((minute * 6) - (hour * 30)).abs
+	angle = ((minute * 6) - (hour * 30))
+	if angle < 0 #checks if the minute hand is further around the clock
+	  angle = angle.abs + minute * 0.5
+	else 
+	 	angle = angle.abs - minute * 0.5
+	end
 
-	if angle > 180
+	if angle > 180 #ensures the smaller of the two angles is returned
 		angle = 360 - angle
 	end
-	
-	angle
+
+	"#{angle}\xC2\xB0"
 end
+
 clock('12:00')
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -3,30 +3,42 @@ require 'clock_angle'
 
 describe('clock') do
 	it 'returns 0 degrees when given 12:00' do
-		expect(clock('12:00')).to eq 0
+		expect(clock('12:00')).to eq "0.0\xC2\xB0"
 	end
 
 	it 'returns 180 degress when given 18:00' do
-		expect(clock('18:00')).to eq 180
+		expect(clock('18:00')).to eq "180.0\xC2\xB0"
 	end
 
 	it 'returns the smallest angle for a whole hour input' do
-		expect(clock('19:00')).to eq 150
+		expect(clock('19:00')).to eq "150.0\xC2\xB0"
 	end
 
 	it 'returns correct angle for inputs with half hours' do
-		expect(clock('12:30')).to eq 180
+		expect(clock('12:30')).to eq "165.0\xC2\xB0"
 	end
 
 	it 'returns correct angle for 11:20' do
-		expect(clock('11:20')).to eq 150
+		expect(clock('11:20')).to eq "140.0\xC2\xB0"
 	end
 
-	it 'returns correct angle for 21:45' do
-		expect(clock('21:45')).to eq 0
+	it 'returns correct angle for 22:30' do
+		expect(clock('22.30')).to eq "135.0\xC2\xB0"
 	end
 
-	it 'returns correct angle for 19:11' do
-		expect(clock('19:11')).to eq 144
+	it 'returns correct angle for 15:40' do
+		expect(clock('15:40')).to eq "130.0\xC2\xB0"
+	end
+
+	it 'accounts for hour hand movement' do
+		expect(clock('5:15')).to eq "67.5\xC2\xB0"
+	end
+
+	it 'returns correct angle for 2:30' do
+		expect(clock('2:30')).to eq "105.0\xC2\xB0"
+	end
+
+	it 'returns correct angle for 8:45' do
+		expect(clock('8:45')).to eq "7.5\xC2\xB0"
 	end
 end
